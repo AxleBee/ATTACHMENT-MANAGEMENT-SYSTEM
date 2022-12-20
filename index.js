2a50491525b13547dde670c76c09f1a43dbbfe52
wanguimbutu <92292540+wanguimbutu@users.noreply.github.com>
2022-12-20 23:19:02 +0300

    Add files via upload
    Adds the API

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { theme } from './theme';
+const express=require('express');
+const connectDB=require('./config/db')
+const app=express();
+//connect the database
+connectDB();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/',(req,res)=>res.send('HELLO WORLD'));
+const port=process.env.PORT || 8082;
+app.listen(port,()=>console.log(`Server is running on port ${port}`));
