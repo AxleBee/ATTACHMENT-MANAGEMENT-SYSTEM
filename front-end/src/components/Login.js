689b6af4f856dba9c6fd5a91df9476553b195632
Daniel Karani <danielevans.karani@gmail.com>
2022-12-22 15:03:21 +0300

    Adds a Login page

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./css/Style.css";
+import "./css/style.css";
 function Login() {
   var x = document.getElementById("login");
   var y = document.getElementById("signup");
